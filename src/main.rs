34a62d932682fa1f5e83ee9d20ae2c9496af03fc
Andrew Bowen <andrew.james.bowen@gmail.com>
2023-04-02 15:27:10 +1000

    Normalize movement

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,17 @@
-use macroquad::prelude::*;
 use cgmath::*;
+use macroquad::prelude::*;
 
 #[macroquad::main("PUBG 2D")]
 async fn main() {
-    let player_speed:f32 = 0.1;
+    let player_speed: f32 = 0.1;
 
+    // TODO: replace with structure of vector2, health, ammo, etc
     let mut player_health: i32 = 100;
     let mut player_ammo: i32 = 60;
 
-    // TODO: replace with structure of vector2, health, ammo, etc
     let mut player_target: Vector2<f32> = Vector2::new(100.0, screen_height() - 50.0);
-    let mut player_x: f32 = player_target.x;
-    let mut player_y: f32 = player_target.y;
-
-    let mut enemy_x: f32 = screen_width() - 50.0;
-    let mut enemy_y: f32 = 100.0;
+    let mut player: Vector2<f32> = Vector2::new(player_target.x, player_target.y);
+    let mut enemy: Vector2<f32> = Vector2::new(screen_width() - 50.0, 100.0);
 
     loop {
         clear_background(BLACK);
@@ -32,52 +29,54 @@ async fn main() {
         draw_text(&health_text, 110.0, 40.0, 20.0, LIGHTGRAY);
         draw_text(&ammo_text, 110.0, 60.0, 20.0, LIGHTGRAY);
 
-        if is_key_pressed(KeyCode::H){
+        if is_key_pressed(KeyCode::H) {
             player_health -= 1;
         }
 
-        if is_key_pressed(KeyCode::Space){
+        if is_key_pressed(KeyCode::Space) {
             player_ammo -= 1;
         }
 
         // Players
-        if player_target.x > player_x {
-            player_x += player_speed;
-        } else if player_target.x < player_x {
-            player_x -= player_speed;
-        }
-        if player_target.y > player_y {
-            player_y += player_speed;
-        } else if player_target.y < player_y {
-            player_y -= player_speed;
+        let player_direction: Vector2<f32> = player_target - player;
+        if player_direction.x != 0.0 || player_direction.y != 0.0 {
+            player += player_direction.normalize() * player_speed;
         }
 
-        draw_circle(player_x, player_y, 5.0, WHITE);
+        draw_circle(player.x, player.y, 5.0, WHITE);
+        draw_line(
+            player_target.x - 5.0,
+            player_target.y - 5.0,
+            player_target.x + 5.0,
+            player_target.y + 5.0,
+            1.0,
+            DARKGRAY,
+        );
+        draw_line(
+            player_target.x - 5.0,
+            player_target.y + 5.0,
+            player_target.x + 5.0,
+            player_target.y - 5.0,
+            1.0,
+            DARKGRAY,
+        );
 
         if is_mouse_button_pressed(MouseButton::Left) {
             let mouse_position = mouse_position();
-            draw_circle(mouse_position.0, mouse_position.1, 5.0, RED);
             player_target.x = mouse_position.0;
             player_target.y = mouse_position.1;
         }
 
         // Enemies
-        if enemy_x > player_x {
-            enemy_x -= player_speed;
-        } else if enemy_x < player_x {
-            enemy_x += player_speed;
+        let enemy_direction: Vector2<f32> = player - enemy;
+        if enemy_direction.x != 0.0 || player_direction.y != 0.0 {
+            enemy += enemy_direction.normalize() * player_speed;
         }
-        if enemy_y > player_y {
-            enemy_y -= player_speed;
-        } else if enemy_y < player_y {
-            enemy_y += player_speed;
-        }
-        // TODO: Normalize the x/y directions
 
-        draw_circle(enemy_x, enemy_y, 5.0, GRAY);
+        draw_circle(enemy.x, enemy.y, 5.0, GRAY);
 
         // UI
-        if is_key_pressed(KeyCode::Q){
+        if is_key_pressed(KeyCode::Q) {
             break;
         }
 
