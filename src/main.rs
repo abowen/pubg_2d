c9d61a990f9ea78daead470a7ac47697e44b2307
Andrew Bowen <andrew.james.bowen@gmail.com>
2023-04-05 11:56:21 +1000

    Multiple enemies

--- src/main.rs
+++ src/main.rs
@@ -11,12 +11,21 @@ async fn main() {
 
     let mut player_target: Vector2<f32> = Vector2::new(100.0, screen_height() - 50.0);
     let mut player: Vector2<f32> = Vector2::new(player_target.x, player_target.y);
-    let mut enemy: Vector2<f32> = Vector2::new(screen_width() - 50.0, 100.0);
 
     // https://doc.rust-lang.org/std/vec/struct.Vec.html
+    let mut enemies: Vec<Enemy> = Vec::new();
     let mut bullets: Vec<Bullet> = Vec::new();
 
+    for i in 0..20 {
+        enemies.push(Enemy {
+            position: Vector2::new(rand::gen_range(200.0, 500.0), 100.0 + i as f32 * 30.0),
+            dead: false,
+        });
+    }
+
     loop {
+        let frame_t = get_time();
+
         clear_background(BLACK);
 
         draw_text("PLAYER 1", 20.0, 20.0, 20.0, LIGHTGRAY);
@@ -33,18 +42,51 @@ async fn main() {
             player_health -= 1;
         }
 
-        if is_key_pressed(KeyCode::Space) {
+        if is_key_pressed(KeyCode::Space) && player_ammo > 0 && enemies.len() > 0 {
             player_ammo -= 1;
+
+            let mut closest_enemy = &enemies[0];
+            let mut closest_distance = 100000.0;
+            for enemy in &enemies {
+                let distance = (enemy.position - player).magnitude();
+                if distance < closest_distance {
+                    closest_distance = distance;
+                    closest_enemy = enemy;
+                }
+            }
+
             bullets.push(Bullet {
                 position: player,
-                direction: player_target - player
+                direction: closest_enemy.position - player,
+                collided: false,
+                shot_at: frame_t,
             });
         }
 
         for bullet in &mut bullets {
             draw_circle(bullet.position.x, bullet.position.y, 2.0, RED);
             bullet.position += bullet.direction.normalize() * 2.0;
+
+            for enemy in &mut enemies {
+                let distance_x: f32 = (enemy.position.x - bullet.position.x).abs();
+                let distance_y: f32 = (enemy.position.y - bullet.position.y).abs();
+                if distance_x < 5.0 && distance_y < 5.0 {
+                    bullet.collided = true;
+                    enemy.dead = true;
+                }
+            }
+
+            if bullet.position.x < 0.0
+                || bullet.position.x > screen_width()
+                || bullet.position.y < 0.0
+                || bullet.position.y > screen_height()
+            {
+                bullet.collided = true;
+            }
         }
+        bullets.retain(|bullet| !bullet.collided);
+        bullets.retain(|bullet| bullet.shot_at + 1.5 > frame_t);
+        enemies.retain(|enemy| !enemy.dead);
 
         // Players
         let player_direction: Vector2<f32> = player_target - player;
@@ -77,12 +119,16 @@ async fn main() {
         }
 
         // Enemies
-        let enemy_direction: Vector2<f32> = player - enemy;
-        if enemy_direction.x != 0.0 || player_direction.y != 0.0 {
-            enemy += enemy_direction.normalize() * player_speed;
+        for enemy in &mut enemies {
+            let enemy_direction: Vector2<f32> = player - enemy.position;
+            if enemy_direction.x != 0.0 || enemy_direction.y != 0.0 {
+                enemy.position += enemy_direction.normalize() * player_speed;
+            }
+
+            draw_circle(enemy.position.x, enemy.position.y, 5.0, GRAY);
         }
 
-        draw_circle(enemy.x, enemy.y, 5.0, GRAY);
+
 
         // UI
         if is_key_pressed(KeyCode::Q) {
@@ -94,7 +140,14 @@ async fn main() {
 }
 
 // https://doc.rust-lang.org/book/ch05-01-defining-structs.html
-struct Bullet{
+struct Bullet {
     position: Vector2<f32>,
-    direction: Vector2<f32>
+    direction: Vector2<f32>,
+    collided: bool,
+    shot_at: f64,
+}
+
+struct Enemy {
+    position: Vector2<f32>,
+    dead: bool,
 }
